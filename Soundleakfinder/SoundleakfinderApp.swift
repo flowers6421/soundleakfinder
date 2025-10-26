a8d142e8bc8f90b595c053f439259ef46257f6cf
Umut Tan <tercumantanumut@gmail.com>
2025-10-26 21:50:38 +0300

    Initial Commit

---
+++ Soundleakfinder/SoundleakfinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundleakfinderApp.swift
+//  Soundleakfinder
+//
+//  Created by Umut Tan on 26.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct SoundleakfinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoundleakfinderTests/SoundleakfinderTests.swift
@@ -0,0 +1,16 @@
+//
+//  SoundleakfinderTests.swift
+//  SoundleakfinderTests
+//
+//  Created by Umut Tan on 26.10.2025.
+//
+
+import Testing
+
+struct SoundleakfinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
